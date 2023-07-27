5f64be0595e64c334e4b18153ad227ac89387c7e
Shreyansh8294 <myid.shreyansh07@gmail.com>
2023-07-27 19:34:23 +0530

    button 1 added

--- app/src/main/java/com/example/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.portfolioapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEducation = findViewById<Button>(R.id.button3)
+
+        buttonEducation.setOnClickListener{
+            intent=Intent(this,educationView::class.java)
+            startActivity(intent)
+        }
     }
 }
