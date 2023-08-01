096bc979adb8dc8db1acc32d2e0e7fc70924e144
Shreyansh8294 <myid.shreyansh07@gmail.com>
2023-08-01 23:20:35 +0530

    Achievements button added

--- app/src/main/java/com/example/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -13,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         val buttonEducation = findViewById<Button>(R.id.button3)
 
         buttonEducation.setOnClickListener{
-            intent=Intent(this,educationView::class.java)
+            intent= Intent(this,educationView::class.java)
+            startActivity(intent)
+        }
+        val buttonAchievements = findViewById<Button>(R.id.button4)
+
+        buttonAchievements.setOnClickListener{
+            intent=Intent(this,achievements::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/portfolioapp/achievements.kt
@@ -0,0 +1,13 @@
+package com.example.portfolioapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class achievements : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_achievements)
+    }
+}
