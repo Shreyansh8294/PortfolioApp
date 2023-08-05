3d3ed74382ac3728b7050caf7a0bd3633ce832e3
Shreyansh8294 <myid.shreyansh07@gmail.com>
2023-08-05 20:52:36 +0530

    Toast added

--- app/src/main/java/com/example/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +16,29 @@ class MainActivity : AppCompatActivity() {
         buttonEducation.setOnClickListener{
             intent= Intent(this,educationView::class.java)
             startActivity(intent)
+            Toast.makeText(this@MainActivity, "Education Details", Toast.LENGTH_SHORT).show()
         }
+
+
         val buttonAchievements = findViewById<Button>(R.id.button4)
 
         buttonAchievements.setOnClickListener{
             intent=Intent(this,achievements::class.java)
             startActivity(intent)
+            Toast.makeText(this@MainActivity, "Achievements", Toast.LENGTH_SHORT).show()
         }
+
+        val buttonSkill= findViewById<Button>(R.id.btnSkills)
+
+        buttonSkill.setOnClickListener{
+            Toast.makeText(this@MainActivity,"Skills",Toast.LENGTH_SHORT).show()
+        }
+
+        val buttonWork= findViewById<Button>(R.id.button5)
+
+        buttonWork.setOnClickListener{
+            Toast.makeText(this@MainActivity,"Work",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
